feb2f5ad670376c84da131a83172aaa28a1a3115
xivLi <13191800224@163.com>
2021-05-12 11:10:08 +0800

    update leet1323-app

--- leet999_1323_6和9组成的最大数字/src/App.java
+++ leet999_1323_6和9组成的最大数字/src/App.java
@@ -5,25 +5,17 @@ public class App {
 }
 
 class Solution {
-    public int[] xorQueries(int[] arr, int[][] queries) {
-        // 返回数组
-        int[] ans = new int[queries.length];
-        // 计算 0-i 的异或
-        int[] xorArr = new int[arr.length];
-        xorArr[0] = arr[0];
-        for (int i = 1; i < xorArr.length; i++) {
-            xorArr[i] = xorArr[i - 1] ^ arr[i];
-        }
-        for (int i = 0; i < queries.length; i++) {
-            int left = queries[i][0];
-            int right = queries[i][1];
-            if (left > 0) {
-                ans[i] = xorArr[left - 1] ^ xorArr[right];
-            } else {
-                ans[i] = xorArr[right];
+    public int maximum69Number(int num) {
+        String str = String.valueOf(num);
+        int i = 0;
+        for (; i < str.length(); i++) {
+            if (str.charAt(i) == '6') {
+                break;
             }
-
         }
-        return ans;
+        if (i < str.length()) {
+            num += 3 * Math.pow(10, str.length() - i - 1);
+        }
+        return num;
     }
 }
