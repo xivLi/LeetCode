9cc5af46328b33fad6758437b0466237198298ac
xivLi <13373276585@163.com>
2021-05-17 22:02:56 +0800

    add leet208

---
+++ leet208_实现Trie/src/App.java
@@ -0,0 +1,68 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    }
+}
+
+class Trie {
+    private Trie[] children;
+    private boolean isEnd;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        if (node.isEnd) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such: Trie obj = new
+ * Trie(); obj.insert(word); boolean param_2 = obj.search(word); boolean param_3
+ * = obj.startsWith(prefix);
+ */
