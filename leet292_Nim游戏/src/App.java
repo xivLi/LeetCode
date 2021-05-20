78d6f30d5c4ba5678268986f2279f83b1b277108
xivLi <13191800224@163.com>
2021-05-20 16:17:23 +0800

    add leet292

---
+++ leet292_Nim游戏/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    }
+}
+
+class Solution {
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+}
