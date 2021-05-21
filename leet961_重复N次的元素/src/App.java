6f3cd298cc48113736c5e3ec2bab0705a073985f
xivLi <13191800224@163.com>
2021-05-21 15:17:55 +0800

    add leet961

---
+++ leet961_重复N次的元素/src/App.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
+
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int ans = 0;
+        set.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                ans = nums[i];
+                break;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return ans;
+    }
+}
